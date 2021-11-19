59d31c29afb538b3fa491b22804fb38804bb167b
Tricia Brooker <grimmi5894@gmail.com>
2021-11-19 16:40:24 -0500

    added loop to check for lowercase, uppercase, numeric, special characters. all tests now pass

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const validatePassword = (password) => {
   if (password.length < 8) return false
+
+  let upper = 0
+  let lower = 0
+  let numeric = 0
+  let special = 0
+
+  for (let i = 0; i < password.length; i++) {
+    if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
+      lower++
+    } else if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) {
+      upper++
+    } else if (!isNaN(password[i])) {
+      numeric++
+    } else if (password.charCodeAt(i) >= 33 && password.charCodeAt(i) <= 47 ||
+        password.charCodeAt(i) >= 58 && password.charCodeAt(i) <= 64 ||
+        password.charCodeAt(i) >= 91 && password.charCodeAt(i) <= 96 ||
+        password.charCodeAt(i) >= 123 && password.charCodeAt(i) <= 126) {
+      special++
+    }
+  }
+
+  return lower > 0 && upper > 0 && numeric > 0 && special > 0
 }
 
 module.exports = validatePassword
